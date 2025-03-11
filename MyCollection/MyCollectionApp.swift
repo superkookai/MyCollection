a82f7c21e6b0ca4826fc65d9f950d929d8c4e07e
superkookai <weerawut@live.com>
2025-03-11 16:29:28 +0700

    Initial Commit

---
+++ MyCollection/MyCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionApp.swift
+//  MyCollection
+//
+//  Created by Weerawut Chaiyasomboon on 11/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct MyCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
